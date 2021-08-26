1acbf58ec7563e3ba937a185b282d8fd26a7b708
kleber gomes da silva <kleber.gsilva@outlook.com>
2021-08-26 12:04:20 -0300

    Criado a class posicao

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Tabuleiro;
+using System;
 
 namespace Xadrez
 {
---
+++ Tabuleiro/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+
+    }
+}
