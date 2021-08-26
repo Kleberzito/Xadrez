a25b8cd4d5cd9593590378c0ea2ded15c68e3d61
kleber gomes da silva <kleber.gsilva@outlook.com>
2021-08-26 16:29:48 -0300

    Introdução de posicao do tabuleiro

---
+++ Chess/PositionChess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tabuleiro;
+
+namespace Chess
+{
+    class PositionChess
+    {
+        public int Linha { get; set; }
+        public char Coluna { get; set; }
+
+        public PositionChess(int linha, char coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public Position toPosition()
+        {
+            return new Position(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return Coluna.ToString().ToUpper() + Linha;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,19 +8,10 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board Boa = new Board(8, 8);
-                Boa.putPiece(new Tower(Boa, Color.Preto), new Position(0, 0));
-                Boa.putPiece(new Tower(Boa, Color.Preto), new Position(1, 3));
-                Boa.putPiece(new King(Boa, Color.Preto), new Position(7, 5));
+            PositionChess p = new PositionChess(7, 'c');
 
-                Screen.printScreen(Boa);
-            }
-            catch(BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(p.toPosition());
+            Console.WriteLine( p);
 
             Console.ReadLine();
         }
